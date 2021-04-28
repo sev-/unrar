9d1ed4e80166e1b23b6fd28d4555c9d6e970b93c
Eugene Sandulenko <sev@scummvm.org>
2021-04-28 22:10:44 +0200

    Merged recursive .cpp includes

--- consio.cpp
+++ consio.cpp
@@ -1,11 +1,30 @@
-/***** File: consio.cpp *****/
-
-
 #include "rar.hpp"
 
 #ifndef GUI
-#include "log.cpp"
+/***** File: log.cpp *****/
+
+static void WriteToLog(const char *ArcName, const char *Message);
+
+static char LogName[NM];
+
+void InitLogOptions(char *LogName) {
+	strcpy(::LogName, LogName);
+}
+
+
+void Log(const char *ArcName, const char *Format, ...) {
+	char Msg[4096];
+	va_list ArgPtr;
+	va_start(ArgPtr, Format);
+	vsprintf(Msg, Format, ArgPtr);
+	va_end(ArgPtr);
+	eprintf("%s", Msg);
+}
+
 #endif
+
+
+/***** File: consio.cpp *****/
 
 static void RawPrint(char *Msg, MESSAGE_TYPE MessageType);
 
